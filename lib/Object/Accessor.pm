8bce4a3832bd98c934a1941ca491942aa431295c
Jos Boumans <kane@cpan.org>
2005-08-25 12:25:18 +0000

    Import of KANE/Object-Accessor-0.10 from CPAN.
    gitpan-cpan-distribution: Object-Accessor
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         KANE/Object-Accessor-0.10.tar.gz
    gitpan-cpan-author:       KANE
    gitpan-cpan-maturity:     released

--- lib/Object/Accessor.pm
+++ lib/Object/Accessor.pm
@@ -1,12 +1,18 @@
 package Object::Accessor;
 
 use strict;
-use Carp        qw[carp];
-use vars        qw[$FATAL $DEBUG $AUTOLOAD $VERSION];
+use Carp            qw[carp];
+use vars            qw[$FATAL $DEBUG $AUTOLOAD $VERSION];
+use Params::Check   qw[allow];
+use Data::Dumper;
 
-$VERSION    = '0.03';
+$VERSION    = '0.10';
 $FATAL      = 0;
 $DEBUG      = 0;
+
+use constant VALUE      => 0;       # array index in the hash value
+use constant ALLOW      => 1;       # array index in the hash value
+use constant ANYTHING   => sub { 1 };
 
 =head1 NAME
 
@@ -18,13 +24,16 @@ Object::Accessor
     $object = Object::Accessor->new;        # create object
 
     $bool   = $object->mk_accessors('foo'); # create accessors
-    $clone  = $object->mk_clone;            # create a clone of the original
+    $bool   = $object->mk_accessors(        # create accessors with input
+                { foo => ALLOW_HANDLER } ); # validation
+                
+    $clone  = $object->mk_clone;            # create a clone of original
                                             # object without data
-    $bool   = $object->mk_flush;            # clean out all data    
-    
-    @list   = $object->ls_accessors;        # retrieves a list of all 
+    $bool   = $object->mk_flush;            # clean out all data
+
+    @list   = $object->ls_accessors;        # retrieves a list of all
                                             # accessors for this object
-    
+
     $bar    = $object->foo('bar');          # set 'foo' to 'bar'
     $bar    = $object->foo();               # retrieve 'bar' again
 
@@ -36,7 +45,7 @@ Object::Accessor
     ### using the object as base class
     package My::Class;
     use base 'Object::Accessor';
-    
+
     $object     = My::Class->new;               # create base object
     $bool       = $object->mk_accessors('foo'); # create accessors, etc...
 
@@ -47,9 +56,29 @@ Object::Accessor
     ### enable debugging
     $Object::Accessor::DEBUG = 1;
 
+
+    ### advanced usage -- scoped attribute values
+    {   my $obj = Object::Accessor->new;
+        
+        $obj->mk_accessors('foo');
+        $obj->foo( 1 );
+        print $obj->foo;                # will print 1
+
+        ### bind the scope of the value of attribute 'foo'
+        ### to the scope of '$x' -- when $x goes out of 
+        ### scope, 'foo's previous value will be restored
+        {   $obj->foo( 2 => \my $x );
+            print $obj->foo, ' ', $x;   # will print '2 2'
+        }
+        print $obj->foo;                # will print 1
+    }
+
+
+
+
 =head1 DESCRIPTION
 
-C<Object::Accessor> provides an interface to create per object 
+C<Object::Accessor> provides an interface to create per object
 accessors (as opposed to per C<Class> accessors, as, for example,
 C<Class::Accessor> provides).
 
@@ -65,25 +94,62 @@ See the C<SYNOPSIS> for examples.
 Creates a new (and empty) C<Object::Accessor> object. This method is
 inheritable.
 
-=cut 
+=cut
 
 sub new {
     return bless {}, $_[0];
 }
 
-=head2 $bool = $object->mk_accessors( @ACCESSORS );
+=head2 $bool = $object->mk_accessors( @ACCESSORS | \%ACCESSOR_MAP );
 
 Creates a list of accessors for this object (and C<NOT> for other ones
-in the same class!). 
-Will not clobber existing data, so if an accessor already exists, 
+in the same class!).
+Will not clobber existing data, so if an accessor already exists,
 requesting to create again is effectively a C<no-op>.
+
+When providing a C<hashref> as argument, rather than a normal list,
+you can specify a list of key/value pairs of accessors and their
+respective input validators. The validators can be anything that
+C<Params::Check>'s C<allow> function accepts. Please see its manpage
+for details.
+
+For example:
+
+    $object->mk_accessors( {
+        foo     => qr/^\d+$/,       # digits only
+        bar     => [0,1],           # booleans
+        zot     => \&my_sub         # a custom verification sub
+    } );        
 
 Returns true on success, false on failure.
 
-Accessors that are called on an object, that no do exist return 
+Accessors that are called on an object, that do not exist return
 C<undef> by default, but you can make this a fatal error by setting the
-global variable C<$FATAL> to true. See the section on C<GLOBAL 
+global variable C<$FATAL> to true. See the section on C<GLOBAL
 VARIABLES> for details.
+
+Note that you can bind the values of attributes to a scope. This allows
+you to C<temporarily> change a value of an attribute, and have it's 
+original value restored up on the end of it's bound variable's scope;
+
+For example, in this snippet of code, the attribute C<foo> will 
+temporarily be set to C<2>, until the end of the scope of C<$x>, at 
+which point the original value of C<1> will be restored.
+
+    my $obj = Object::Accessor->new;
+    
+    $obj->mk_accessors('foo');
+    $obj->foo( 1 );
+    print $obj->foo;                # will print 1
+
+    ### bind the scope of the value of attribute 'foo'
+    ### to the scope of '$x' -- when $x goes out of 
+    ### scope, 'foo' previous value will be restored
+    {   $obj->foo( 2 => \my $x );
+        print $obj->foo, ' ', $x;   # will print '2 2'
+    }
+    print $obj->foo;                # will print 1
+    
 
 Note that all accessors are read/write for everyone. See the C<TODO>
 section for details.
@@ -93,22 +159,29 @@ section for details.
 sub mk_accessors {
     my $self = $_[0];
     
-    for my $acc (@_[1..$#_]) {
-    
+    ### first argument is a hashref, which means key/val pairs
+    ### as keys + allow handlers
+    my %hash = UNIVERSAL::isa( $_[1], 'HASH' )
+                ? %{$_[1]}
+                : map { $_ => ANYTHING } @_[1..$#_]; 
+        
+    while( my($acc,$allow) = each %hash ) {
+
         ### already created apparently
         if( exists $self->{$acc} ) {
             _debug( "Accessor '$acc' already exists");
             next;
-        }            
-    
+        }
+
         _debug( "Creating accessor '$acc'");
-    
+
         ### explicitly vivify it, so that exists works in ls_accessors()
-        $self->{$acc} = undef;
+        $self->{$acc}->[VALUE] = undef;
+        $self->{$acc}->[ALLOW] = $allow;        
     }
-    
+
     return 1;
-}    
+}
 
 =head2 @list = $self->ls_accessors;
 
@@ -120,7 +193,20 @@ by one to the C<can> method.
 
 sub ls_accessors {
     return sort keys %{$_[0]};
-}    
+}
+
+=head2 $ref = $self->ls_allow(KEY)
+
+Returns the allow handler for the given key, which can be used with
+C<Params::Check>'s C<allow()> handler.
+
+=cut
+
+sub ls_allow {
+    my $self = shift;
+    my $key  = shift or return;
+    return $self->{$key}->[ALLOW];
+}
 
 =head2 $clone = $self->mk_clone;
 
@@ -132,12 +218,13 @@ accessors as the current object, but without the data stored in them.
 sub mk_clone {
     my $self    = $_[0];
     my $class   = ref $self;
-    
+
     my $clone   = $class->new;
-    
+    my %hash    = map { $_ => $self->ls_allow($_) } $self->ls_accessors;
+
     # copy the accessors from $self to $clone
-    $clone->mk_accessors( $self->ls_accessors );
-    
+    $clone->mk_accessors( \%hash );
+
     return $clone;
 }
 
@@ -154,15 +241,40 @@ sub mk_flush {
     my $self = $_[0];
 
     # set each accessor's data to undef
-    $self->$_( undef ) for $self->ls_accessors;
-    
+    $self->{$_}->[VALUE] = undef for $self->ls_accessors;
+
     return 1;
-}    
+}
+
+=head2 $bool = $self->mk_verify;
+
+Checks if all values in the current object are in accordance with their
+own allow handler. Specifically useful to check if an empty initialised
+object has been filled with values satisfying their own allow criteria.
+
+=cut
+
+sub mk_verify {
+    my $self = $_[0];
+    
+    my $fail;
+    for my $name ( $self->ls_accessors ) {
+        unless( allow( $self->$name, $self->{$name}->[ALLOW] ) ) {
+            my $val = defined $self->$name ? $self->$name : '<undef>';
+
+            _error("'$name' ($val) is invalid");
+            $fail++;
+        }
+    }
+
+    return if $fail;
+    return 1;
+}   
 
 =head2 $bool = $self->can( METHOD_NAME )
 
 This method overrides C<UNIVERAL::can> in order to provide coderefs to
-accessors which are loaded on demand. It will behave just like 
+accessors which are loaded on demand. It will behave just like
 C<UNIVERSAL::can> where it can -- returning a class method if it exists,
 or a closure pointing to a valid accessor of this particular object.
 
@@ -171,8 +283,8 @@ You can use it as follows:
     $sub = $object->can('some_accessor');   # retrieve the coderef
     $sub->('foo');                          # 'some_accessor' now set
                                             # to 'foo' for $object
-    $foo = $sub->();                        # retrieve the contents 
-                                            # of 'foo'                                            
+    $foo = $sub->();                        # retrieve the contents
+                                            # of 'some_accessor'
 
 See the C<SYNOPSIS> for more examples.
 
@@ -181,19 +293,19 @@ See the C<SYNOPSIS> for more examples.
 ### custom 'can' as UNIVERSAL::can ignores autoload
 sub can {
     my($self, $method) = @_;
-    
+
     ### it's one of our regular methods
     if( $self->UNIVERSAL::can($method) ) {
         _debug( "Can '$method' -- provided by package" );
         return $self->UNIVERSAL::can($method);
     }
-    
+
     ### it's an accessor we provide;
     if( exists $self->{$method} ) {
         _debug( "Can '$method' -- provided by object" );
-        return sub { $self->$method(@_); } 
-    }        
-    
+        return sub { $self->$method(@_); }
+    }
+
     ### we don't support it
     _debug( "Cannot '$method'" );
     return;
@@ -202,43 +314,110 @@ sub can {
 ### don't autoload this
 sub DESTROY { 1 };
 
-### use autoload so we can have per-object accessors, 
+### use autoload so we can have per-object accessors,
 ### not per class, as that is incorrect
 sub AUTOLOAD {
-    my $self    = $_[0];
+    my $self    = shift;
     my($method) = ($AUTOLOAD =~ /([^:']+$)/);
-    
+
     unless( exists $self->{$method} ) {
         _error("No such accessor '$method'");
         return;
-    }        
+    }
 
-    ### if there's more arguments than $self, then
-    ### replace the method called by the accessor.
-    ### XXX implement rw vs ro accessors!
-    $self->{$method} = $_[1] if @_ > 1;
+
+    ### assign?
+    if( @_ ) {
+        my $val = shift;
+
+        ### any binding?
+        if( $_[0] ) {
+            if( ref $_[0] and UNIVERSAL::isa( $_[0], 'SCALAR' ) ) {
+            
+                ### tie the reference, so we get an object and
+                ### we can use it's going out of scope to restore
+                ### the old value
+                my $cur = $self->{$method}->[VALUE];
+                
+                tie ${$_[0]}, __PACKAGE__ . '::TIE', 
+                        sub { $self->$method( $cur ) };
     
-    return $self->{$method};
+                ${$_[0]} = $val;
+            
+            } else {
+                _error( "Can not bind '$method' to anything but a SCALAR" );
+            }
+        }
+        
+        ### need to check the value?
+        unless( $self->{$method}->[ALLOW] eq ANYTHING ) {
+            ### double assignment due to 'used only once' warnings
+            local $Params::Check::VERBOSE = 0;
+            local $Params::Check::VERBOSE = 0;
+            
+            allow( $val, $self->{$method}->[ALLOW] ) or (
+                _error( "'$_[1]' is an invalid value for '$method'"), 
+                return 
+            ); 
+        }
+
+        ### if there's more arguments than $self, then
+        ### replace the method called by the accessor.
+        ### XXX implement rw vs ro accessors!
+        $self->{$method}->[VALUE] = $val;
+    }
+
+    return $self->{$method}->[VALUE];
 }
 
-sub _debug { 
+
+sub _debug {
     return unless $DEBUG;
 
     local $Carp::CarpLevel += 1;
-    carp(@_); 
+    carp(@_);
 }
 
-sub _error { 
+sub _error {
     local $Carp::CarpLevel += 1;
-    $FATAL ? croak(@_) : carp(@_); 
+    $FATAL ? croak(@_) : carp(@_);
 }
 
-=head1 GLOBAL VARIABLES 
+### standard tie class for bound attributes
+{   package Object::Accessor::TIE;
+    use Tie::Scalar;
+    use Data::Dumper;
+    use base 'Tie::StdScalar';
+
+    my %local = ();
+
+    sub TIESCALAR {
+        my $class   = shift;
+        my $sub     = shift;
+        my $ref     = undef;
+        my $obj     =  bless \$ref, $class;
+
+        ### store the restore sub 
+        $local{ $obj } = $sub;
+        return $obj;
+    }
+    
+    sub DESTROY {
+        my $tied    = shift;
+        my $sub     = delete $local{ $tied };
+
+        ### run the restore sub to set the old value back
+        return $sub->();        
+    }              
+}
+
+
+=head1 GLOBAL VARIABLES
 
 =head2 $Object::Accessor::FATAL
 
 Set this variable to true to make all attempted access to non-existant
-accessors be fatal. 
+accessors be fatal.
 This defaults to C<false>.
 
 =head2 $Object::Accessor::DEBUG
@@ -261,7 +440,7 @@ Jos Boumans E<lt>kane@cpan.orgE<gt>.
 =head1 COPYRIGHT
 
 This module is
-copyright (c) 2004 Jos Boumans E<lt>kane@cpan.orgE<gt>.
+copyright (c) 2004-2005 Jos Boumans E<lt>kane@cpan.orgE<gt>.
 All rights reserved.
 
 This library is free software;
--- t/00_Object-Accessor.t
+++ t/00_Object-Accessor.t
@@ -21,12 +21,12 @@ $Object::Accessor::DEBUG = $Object::Accessor::DEBUG = 1 if @ARGV;
 ### check the object
 {   ok( $Object,                "Object of '$Class' created" );
     isa_ok( $Object,            $Class );
-}    
+}
 
 ### check non existant accessor
 {   my $warning;
     local $SIG{__WARN__} = sub { $warning .= "@_" };
-   
+
     ok(!$Object->can($Acc),     "Cannot '$Acc'" );
     ok(!$Object->$Acc(),        "   Method '$Acc' returns false" );
     like( $warning, $Err_re,    "   Warning logged" );
@@ -34,9 +34,9 @@ $Object::Accessor::DEBUG = $Object::Accessor::DEBUG = 1 if @ARGV;
     ### check fatal error
     {   local $Object::Accessor::FATAL = 1;
         local $Object::Accessor::FATAL = 1; # stupid warnings
-        
+
         my $rv = eval { $Object->$Acc() };
-        
+
         ok( $@,                 "Cannot '$Acc' -- dies" );
         ok(!$rv,                "   Method '$Acc' returns false" );
         like( $@, $Err_re,      "   Fatal error logged" );
@@ -49,34 +49,34 @@ $Object::Accessor::DEBUG = $Object::Accessor::DEBUG = 1 if @ARGV;
 
     ok( $Object->mk_accessors( $Acc ),
                                 "Accessor '$Acc' created" );
-                                
-    ok( $Object->can( $Acc ),   "   Can '$Acc'" );                                
+
+    ok( $Object->can( $Acc ),   "   Can '$Acc'" );
     ok(!$warning,               "   No warnings logged" );
 }
 
-### try to use the accessor 
+### try to use the accessor
 {   for my $var ($0, $$) {
 
         ok( $Object->$Acc( $var ),  "'$Acc' set to '$var'" );
         is( $Object->$Acc(), $var,  "   '$Acc' still holds '$var'" );
-    
+
         my $sub = $Object->can( $Acc );
         ok( $sub,                   "Retrieved '$Acc' coderef" );
         isa_ok( $sub,               "CODE" );
         is( $sub->(), $var,         "   '$Acc' via coderef holds '$var'" );
-    
+
         ok( $sub->(1),              "   '$Acc' set via coderef to '1'" );
         is( $Object->$Acc(), 1,     "   '$Acc' still holds '1'" );
     }
-}    
+}
 
 ### get a list of accessors
 {   my @list = $Object->ls_accessors;
     ok( scalar(@list),              "Accessors retrieved" );
-    
+
     for my $acc ( @list ) {
         ok( $Object->can( $acc ),   "   Accessor '$acc' is valid" );
-    }        
+    }
 
     is_deeply( \@list, [$Acc],      "   Only expected accessors found" );
 }
@@ -84,7 +84,7 @@ $Object::Accessor::DEBUG = $Object::Accessor::DEBUG = 1 if @ARGV;
 ### clone the original
 {   my $clone = $Object->mk_clone;
     my @list  = $clone->ls_accessors;
-    
+
     ok( $clone,                     "Clone created" );
     isa_ok( $clone,                 $Class );
     ok( scalar(@list),              "   Clone has accessors" );
@@ -100,7 +100,7 @@ $Object::Accessor::DEBUG = $Object::Accessor::DEBUG = 1 if @ARGV;
 ### flush the original values
 {   my $val = $Object->$Acc();
     ok( $val,                       "Objects '$Acc' has a value" );
-    
+
     ok( $Object->mk_flush,          "   Object flushed" );
     ok( !$Object->$Acc(),           "   Objects '$Acc' is now empty" );
 }
@@ -108,10 +108,10 @@ $Object::Accessor::DEBUG = $Object::Accessor::DEBUG = 1 if @ARGV;
 ### check that only our original object can do '$Acc'
 {   my $warning;
     local $SIG{__WARN__} = sub { $warning .= "@_" };
-    
+
     my $other = $Class->new;
-    
-    
+
+
     ok(!$other->can($Acc),          "Cannot '$Acc' via other object" );
     ok(!$other->$Acc(),             "   Method '$Acc' returns false" );
     like( $warning, $Err_re,        "   Warning logged" );
